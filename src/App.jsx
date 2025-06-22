35a624649aeeced5fae18037fe0daab8364f5951
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2025-06-22 11:41:02 +0530

    Frontend setup : install react+vite, config tailwind css and Install necessary libraries

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='text-center mt-70'>
+
+    <h1 className="text-4xl font-bold underline">Signature document website</h1>
+
+    </section>
+  )
+}
+
+export default App
